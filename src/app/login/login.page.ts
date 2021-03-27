4edb33f9845fcf609886ad20d1e5e25677df83df
nicolas aramayo <aramayo@gmail.com>
2021-03-27 03:24:36 -0300

    Se crea componente login funcionando

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private authSvc: AuthService) { }
 
   ngOnInit() {
   }
+
+
+  async onLogin(email,password) {
+    try {
+      const user = await this.authSvc.login(email.value,password.value);
+      if(user){
+        // To do: checkingEmail
+        console.log("user", user);
+      }
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  /*async onLoginGoogle(){
+    try {
+      const user = await this.authSvc.loginGoogle....;
+    } catch (error) {
+      console.log(error);
+    }
+  }*/
+
 
 }
